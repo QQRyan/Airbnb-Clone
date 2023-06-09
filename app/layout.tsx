48bf72f30ce7dbc8676fbd9b72b4cb5b443b50ba
QQRyan <ryanliu383@gmail.com>
2023-06-09 04:20:39 -0400

    Update Trips, Favoite, Reservation and Properties

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,33 +11,35 @@ import ToasterProvider from './providers/ToasterProvider';
 import getCurrentUser from './actions/getCurrentUser';
 
 const font = Nunito({
-  subsets: ["latin"]
+    subsets: ["latin"]
 })
 
 export const metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb clone',
+    title: 'Airbnb',
+    description: 'Airbnb clone',
 }
 
 export default async function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  const currentUser = await getCurrentUser();
+    const currentUser = await getCurrentUser();
 
-  return (
-    <html lang="en">
-      <body className={font.className}>
-        <ClientOnly>
-          <ToasterProvider />
-          <RentModal />
-          <LoginModal />
-          <RegisterModal />
-          <Navbar currentUser={currentUser} />
-        </ClientOnly>
-        {children}
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={font.className}>
+                <ClientOnly>
+                    <ToasterProvider />
+                    <RentModal />
+                    <LoginModal />
+                    <RegisterModal />
+                    <Navbar currentUser={currentUser} />
+                </ClientOnly>
+                <div className="pb-20 pt-28">
+                    {children}
+                </div>
+            </body>
+        </html>
+    )
 }
